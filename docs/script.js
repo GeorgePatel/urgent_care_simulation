5de4fe1aa8d57878026f47531d24808c7c91f3ac
Gunjan Patel <gunjanpatel@Gunjans-MacBook-Air.local>
2025-01-09 19:27:29 -0500

    Update script to fetch file from S3

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,9 @@ document.getElementById('fetch-output').addEventListener('click', () => {
     const outputElement = document.getElementById('simulation-output');
     outputElement.textContent = 'Fetching output...';
 
-    fetch('simulationOut.txt')
+    const fileUrl = 'https://urgentcare-simulation-output.s3.us-east-1.amazonaws.com/simulationOut.txt'
+
+    fetch(fileUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch: ${response.status}`);
